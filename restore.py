0bce1a72e2f67cb7374b85858526bf9a2429c604
Ranit Varma <ranitkadel8381@gmail.com>
2026-03-02 02:32:37 +0530

    Delete restore.py

--- restore.py
+++
@@ -1,8 +0,0 @@
-import zipfile
-import os
-
-def restore_backup(backup_path, extract_to):
-    with zipfile.ZipFile(backup_path, 'r') as zipf:
-        zipf.extractall(extract_to)
-
-    print("Restore Completed ")
